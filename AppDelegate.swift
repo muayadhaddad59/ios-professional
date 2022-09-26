e35b314465cdd514ba8c37b9b000115d8765cb53
Muayad Haddad <muayadhaddad@yahoo.com>
2022-09-26 15:29:51 +0300

    First Deploy

---
+++ AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Muayad Haddad on 26/09/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .orange
+        window?.rootViewController = LoginViewController()
+
+        return true
+    }
+
+}
+
---
+++ Bankey/Login/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  Bankey
+//
+//  Created by Muayad Haddad on 26/09/2022.
+//
+
+import Foundation
